8ce2dcfd30bd12bfa20be5065e6c5ee6bdda64b8
Daniel PC <daniel.barczynski@onet.pl>
2022-04-27 17:25:02 +0200

    init

---
+++ Pudelko/Pudelko/Pudelko.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pudelko
+{
+    public class Pudelko
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public enum UnitOfMeasure
+        {
+            milimeter, centimeter, meter
+        }
+
+        private UnitOfMeasure _unit;
+        public Pudelko(int a, int b, int c, UnitOfMeasure unit)
+        {
+            A = a;
+            B = b;
+            C = c;
+            _unit = unit;
+        }
+
+
+    }
+}
